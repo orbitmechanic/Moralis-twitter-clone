f7195beaa8f9deb2206c2921cae78bfba58c634e
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2021-06-11 09:36:29 -0700

    Authentication Sign in/up, login

--- src/App.js
+++ src/App.js
@@ -1,48 +1,25 @@
-import {
-  Button,
-  Alert,
-  AlertIcon,
-  Box,
-  AlertTitle,
-  AlertDescription,
-  CloseButton,
-} from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 import { Container, Heading } from "@chakra-ui/layout";
 import { useMoralis } from "react-moralis";
-
+import { Auth } from "./Auth";
 function App() {
-  const { authenticate, isAuthenticated, isAuthenticating, authError, logout } =
-    useMoralis();
+  const { isAuthenticated, logout } = useMoralis();
 
   if (isAuthenticated) {
     return (
       <Container>
-        <Heading>Welcome to the twitter clone</Heading>
-        <Button onClick={() => logout()}>Log Out</Button>
+        <Heading>Welcome to the Twitter Clone</Heading>
+        <Button onClick={() => logout()}>Logout</Button>
+      </Container>
+    );
+  } else {
+    return (
+      <Container>
+        <Heading mb={6}>Welcome to the Twitter Clone</Heading>
+        <Auth />
       </Container>
     );
   }
-
-  return (
-    <Container>
-      Twitter Clone
-      {authError && (
-        <Alert status="error">
-          <AlertIcon />
-          <Box flex="1">
-            <AlertTitle>Authentication has failed.</AlertTitle>
-            <AlertDescription display="block">
-              {authError.message}
-            </AlertDescription>
-          </Box>
-          <CloseButton position="absolute" right="8px" top="8px" />
-        </Alert>
-      )}
-      <Button isLoading={isAuthenticating} onClick={() => authenticate()}>
-        Authenticate
-      </Button>
-    </Container>
-  );
 }
 
 export default App;
